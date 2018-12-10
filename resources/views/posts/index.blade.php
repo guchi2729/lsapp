990ec2d506cee7669fad36d921c7fb1483bf0c81
Guchi <kawaguchi@mango-byte.com>
2018-12-10 14:30:07 +0700

    fixed some layouts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,8 +8,10 @@
             <div class="card">
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-md-4 col-sm-4">
-                            <img style="width:100%" src="/storage/cover_images/{{$post->cover_image}}">
+                        <div class="col-md-4 col-sm-4" style="height:150px">
+                            <a href="/posts/{{$post->id}}">
+                            <img style="width:100%; max-width:300px; max-height:150px" src="/storage/cover_images/{{$post->cover_image}}">
+                            </a>
                         </div>
                         <div class="col-md-8 col-sm-8">
                             <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
